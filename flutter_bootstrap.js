9fc7c8ab7c780e5a632a19551ed9ff36c32601a0
Kaviya-relatim <Kaviya-relatim@users.noreply.github.com>
2026-01-06 13:39:38 +0000

    deploy: feb5c34b347bc82791aaa53ee1eecf1def857efc

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2364756256"
+    serviceWorkerVersion: "2632191329"
   }
 });
