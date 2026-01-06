7869bab995813086d42bc148b78354502e9a3278
Kaviya-relatim <kaviya.priya@relatim.in>
2026-01-06 21:43:24 +0530

    bug

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,12 +2,12 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
-    id("dev.flutter.flutter-gradle-plugin")
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0"
 }
 
 android {
     namespace = "com.example.relatim"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -18,14 +18,20 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
+    
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+    }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.relatim"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 21
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -35,6 +41,15 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+    
+    // Ensure the build uses the correct Kotlin version
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
         }
     }
 }
@@ -43,10 +58,16 @@ flutter {
     source = "../.."
 }
 
-configurations.all {
-    resolutionStrategy {
-        force("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.24")
-        force("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.24")
+// Ensure the Flutter Gradle Plugin is properly configured
+afterEvaluate {
+    if (project.plugins.hasPlugin("dev.flutter.flutter-gradle-plugin")) {
+        println("Flutter Gradle Plugin is properly applied")
+    } else {
+        println("Warning: Flutter Gradle Plugin may not be properly applied")
     }
+}
+
+dependencies {
+    // The Flutter Gradle Plugin automatically adds the Flutter embedding dependencies
+    // No need to specify them manually
 }
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,9 +18,17 @@ pluginManagement {
 }
 
 plugins {
+    id("com.android.application") version "8.6.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+}
 
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
+    }
 }
 
 include(":app")
