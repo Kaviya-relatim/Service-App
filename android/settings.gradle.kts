feb5c34b347bc82791aaa53ee1eecf1def857efc
Kaviya-relatim <kaviya.priya@relatim.in>
2026-01-06 19:08:12 +0530

    Downgrade AGP and Kotlin to stable versions

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,8 +19,8 @@ pluginManagement {
 
 plugins {
 
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application") version "8.3.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 include(":app")
